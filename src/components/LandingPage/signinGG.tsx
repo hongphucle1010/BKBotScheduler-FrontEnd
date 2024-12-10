c591584fb82520c98b27b18253978cb2c6f85b66
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2024-12-10 21:35:25 +0700

    feat: refactor Google sign-in button to use useGoogleLogin and add one-tap login

--- src/components/LandingPage/signinGG.tsx
+++ src/components/LandingPage/signinGG.tsx
@@ -1,19 +1,39 @@
-import { GoogleLogin } from '@react-oauth/google'
+// import { GoogleLogin } from '@react-oauth/google'
 
-const signinButtonGG: React.FC = () => {
+// const SigninButtonGG: React.FC = () => {
+//   return (
+//     <GoogleLogin
+//       onSuccess={(credentialResponse) => {
+//         console.log(credentialResponse)
+//       }}
+//       onError={() => {
+//         console.log('Login Failed')
+//       }}
+//       auto_select
+//       shape='pill'
+//     />
+//   )
+// }
+
+// export default SigninButtonGG
+
+import { useGoogleLogin } from '@react-oauth/google'
+import ggLogo from '../../assets/LandingPage/gg-logo.png'
+
+const SigninButtonGG: React.FC = () => {
+  const login = useGoogleLogin({
+    onSuccess: (codeResponse) => {
+      console.log(codeResponse)
+    },
+    hosted_domain: 'hcmut.edu.vn'
+  })
+
   return (
-    <GoogleLogin
-      hosted_domain='hcmut.edu.vn'
-      onSuccess={(credentialResponse) => {
-        console.log(credentialResponse)
-      }}
-      onError={() => {
-        console.log('Login Failed')
-      }}
-      shape='pill'
-      auto_select={true}
-    />
+    <button className='p-2 bg-gray-200 rounded-full font-semibold flex gap-2 items-center' onClick={() => login()}>
+      <img src={ggLogo} alt='google logo' />
+      <span>Sign in with Google</span>
+    </button>
   )
 }
 
-export default signinButtonGG
+export default SigninButtonGG
--- src/pages/LandingPage/LandingPage.tsx
+++ src/pages/LandingPage/LandingPage.tsx
@@ -2,8 +2,18 @@ import React from 'react'
 import logo from '../../assets/LandingPage/Logo.png'
 import studentImg from '../../assets/LandingPage/student.png'
 import SigninButtonGG from '../../components/LandingPage/signinGG.tsx'
+import { useGoogleOneTapLogin } from '@react-oauth/google'
 
 const LandingPage: React.FC = () => {
+  useGoogleOneTapLogin({
+    onSuccess: (credentialResponse) => {
+      console.log(credentialResponse)
+    },
+    onError: () => {
+      console.log('Login Failed')
+    },
+    hosted_domain: 'hcmut.edu.vn'
+  })
   return (
     <div className='flex flex-col justify-between h-screen'>
       <div className='flex items-center justify-between py-6 px-20'>
